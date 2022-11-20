484f12b1c16f72552ba65558a16cdd53fe51551b
Jared Gold <114547220+goldjared@users.noreply.github.com>
2022-11-20 10:45:32 -0500

    Add function for playing 1 round

--- js/external.js
+++ js/external.js
@@ -1,8 +1,61 @@
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissor"];
-    const randomValue = choices[Math.floor(Math.random() * choices.length)];
-    return randomValue;
+    const computerOptions = ['rock', 'paper', 'scissor'];
+    const computerSelection = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    return computerSelection;
 }
-console.log(getComputerChoice())
 
-let playerChoice = prompt("Rock, Paper, or Scissor?", "");
+function getPlayerChoice() {
+    const playerSelection = prompt('Rock, Paper, or Scissor?', '');
+    return playerSelection.toLowerCase();
+    
+}
+
+function playRound(getComputerChoice, getPlayerChoice) {
+    let result;
+    if (getPlayerChoice === 'rock') {
+        switch(true) {
+            case getComputerChoice === 'rock':
+                result = 'Tie';
+                break;
+            case getComputerChoice === 'paper':
+                result = 'Loss';
+                break;
+            case getComputerChoice === 'scissor':
+                result = 'Win!';   
+                break;  
+                    }          
+    } else if (getPlayerChoice === 'paper') {
+        switch(true) {
+            case getComputerChoice === 'rock':
+                result = 'Win!';
+                break;
+            case getComputerChoice === 'paper':
+                result = 'Tie';
+                break;
+            case getComputerChoice === 'scissor':
+                result = 'Loss';   
+                break;  
+                    }
+    } else if (getPlayerChoice === 'scissor') {
+        switch(true) {
+            case getComputerChoice === 'rock':
+                result = 'Loss';
+                break;
+            case getComputerChoice === 'paper':
+                result = 'Win!';
+                break;
+            case getComputerChoice === 'scissor':
+                result = 'Tie';   
+                break;  
+                    } 
+    }  else {
+        result = 'Fatal error. Use a valid hand to play';
+    }         
+    return result;
+
+}
+console.log(playRound(getComputerChoice(), getPlayerChoice()));
+
+function game(playRound) {
+    
+}
