7318c781b4b028d87793499176fc9983910cd22e
Jared Gold <114547220+goldjared@users.noreply.github.com>
2022-12-10 21:44:25 -0500

    Add UI to game

--- js/external.js
+++ js/external.js
@@ -4,48 +4,87 @@ function getComputerChoice() {
 }
 
 //console.log(getComputerChoice());
+/*let result;
+let container = document.querySelector('#container');
+let buttons = document.querySelectorAll('.btn');
+buttons.forEach(function (button) {
+    button.addEventListener('click', function() {
+            console.log(button.value);
+            let playerSelection = button.value;
+            let computerSelection = getComputerChoice();
+            (playRound(playerSelection, computerSelection));
+            const content = document.createElement('div');
+            content.classList.add('content');
+            content.textContent = `${result}`;
+            container.appendChild(content);
+
+
+            //console.log(playRound(playerSelection, computerSelection));
+            
+
+    });
+  }); */
+//https://www.codeinwp.com/snippets/add-event-listener-to-multiple-elements-with-javascript/
+
+let buttons = document.querySelectorAll('.btn');
+buttons.forEach(function (button) {
+    button.addEventListener('click', function() {
+            //console.log('player selection is', button.value);
+            let playerSelection = button.value;
+            let computerSelection = getComputerChoice();
+            //console.log('computerSelection is', computerSelection)
+            //console.log(playRound(playerSelection, computerSelection));
+            let roundResult = playRound(playerSelection, computerSelection);
+            //playRound(playerSelection, computerSelection); not needed?
+            const resultContainer = document.querySelector('#container');
+            const results = document.createElement('div');
+            results.classList.add('results');
+            results.textContent = `Player: ${playerSelection}, CPU: ${computerSelection},
+            RESULT: ${roundResult}`.toUpperCase();
+            resultContainer.appendChild(results);
+            results.style.color = 'red';
+        });
+    });
+
+ 
+
+
+
+
+
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock') {
-        switch(true) {
-            case computerSelection === 'rock':
+        switch(computerSelection) {
+            case 'rock':
                 return 'Tie';
-            case computerSelection === 'paper':
-                return 'Loss. Paper destroys rock.';
-            case computerSelection === 'scissors':
-                return 'Win! Rock crushes scissors!';   
+            case 'paper':
+                return 'Loss';
+            case 'scissors':
+                return 'Win!';   
                     }          
     } else if (playerSelection === 'paper') {
-        switch(true) {
-            case computerSelection === 'rock':
-                return 'Win! Paper destroys rock.';
-            case computerSelection === 'paper':
+        switch(computerSelection) {
+            case 'rock':
+                return 'Win!';
+            case 'paper':
                 return 'Tie';
-            case computerSelection === 'scissors':
-                return 'Loss. Scissors cut paper.';   
+            case 'scissors':
+                return 'Loss';   
                     }
     } else if (playerSelection === 'scissors') {
-        switch(true) {
-            case computerSelection === 'rock':
-                return 'Loss. Rock crushes scissors.';
-            case computerSelection === 'paper':
-                return 'Win! Scissors cut paper.';
-            case computerSelection === 'scissors':
+        switch(computerSelection) {
+            case 'rock':
+                return 'Loss';
+            case 'paper':
+                return 'Win!';
+            case 'scissors':
                 return 'Tie';  
                     } 
     }  else {
         return 'Error. Use a valid hand to play';
     }         
-    return
+    return;
 
-}
-
-
-function game() {
-        for(let i = 0; i < 5; i++) {
-            let playerSelection = prompt('Rock, paper, or scissor?', '').toLowerCase();
-            let computerSelection = getComputerChoice();
-            console.log(playRound(playerSelection, computerSelection));
-
-        }
 }
